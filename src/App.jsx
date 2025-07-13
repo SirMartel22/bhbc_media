91040154b741f6d201353466eb21d6f4aa441913
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-13 01:35:11 +0100

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import bhbcLogo from './assets/logo_white.png'
 import './App.css'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   const [count, setCount] = useState(0)
