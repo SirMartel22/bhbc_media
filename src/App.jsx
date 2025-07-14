790efa11c4a03006440ba5628c69ac989f1f68fa
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-14 14:53:22 +0100

    created the template for needed pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import bhbcLogo from './assets/logo_white.png'
-import './App.css'
+import bhbcLogo from './assets/logo_black.png'
+// import './App.css'
 import { Analytics } from '@vercel/analytics/react'
 
 function App() {
---
+++ src/components/HistoryTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HistoryTable = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HistoryTable
