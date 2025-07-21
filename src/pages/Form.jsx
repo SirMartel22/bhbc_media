9e8ab34741246701d206369e753d6a32a53b09e6
Devtimmy <oyinloyematthew6@gmail.com>
2025-07-21 12:55:27 +0100

    Done with the prototype of the form page

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import ctl from '@netlify/classnames-template-literals';
 
 
 const Form = () => {
+
+  const inputFormStyle = ctl(`
+        bg-[#f4f8f7] 
+        py-1 
+        px-8 
+        rounded-lg 
+        border 
+        text-[#36454F]
+    `)
+  
+  const inputFormDivs = ctl(`
+        flex
+        flex-col
+        gap-2
+    `)
   return (
-    <div className="flex justify-center pt-18 px-48">
+    <div className="flex justify-center pt-8 px-48">
       <div className="form-wrapper overflow-hidden rounded-xl w-full flex justify-center items-center">
 
           <div className="left-side flex flex-col justify-center items-center max-w-2xl bg-[#3bb398] p-12 gap-6 h-[100%] w-xl rounded-l-lg">
@@ -14,47 +30,47 @@ const Form = () => {
           
           </div>
 
-          <div className="right-side flex flex-col justify-center items-center w-2xl max-w-3xl  bg-[#f5f5f5] p-12 gap-6 h-full rounded-r-lg">
+          <div className="right-side flex flex-col justify-center items-left w-2xl max-w-3xl bg-[#f5f5f5] p-8 gap-6 h-full rounded-r-lg">
             <div className="form-title text-[#3eaf9c] font-bold text-2xl">
               Register Your Info
             </div>
-            <form action='/submit' method="POST" className="flex flex-col justify-center items-left gap-2">
-              <div className="name-username-div">
+            <form action='/submit' method="POST" className="flex flex-col justify-center items-left gap-2 ">
+              <div className={inputFormDivs }>
                   <div className="name">
                       {/* <label for ="name" >Name </label> */}
-                      <input type="text" className="" placeholder="Name e.g 'Timmy Oyin'" />  
+                      <input type="text" className={inputFormStyle} placeholder="Name e.g 'Timmy Oyin'" />  
                   </div>
 
 
                   <div className="name">
                       {/* <label for ="name" >Username </label> */}
-                      <input type="text" className="" placeholder="Username eg. 'SirMartel' " />  
+                      <input type="text" className={inputFormStyle} placeholder="Username eg. 'SirMartel' " />  
                   </div>
               </div>
                     
-              <div className="email-phone-div">
+              <div className={inputFormDivs}>
                   <div className="name">
                       {/* <label for ="name">Email Address </label> */}
-                      <input type="email" className="" placeholder="Email e.g 'someone@example.com'" />  
+                      <input type="email" className={inputFormStyle} placeholder="Email e.g 'someone@example.com'" />  
                     </div>
                     
                   <div className="name">
                       {/* <label for ="name">Phone </label> */}
-                      <input type="email" className="" placeholder="Phone '0908 877 6655' " />  
+                      <input type="tel" className={inputFormStyle} placeholder="Phone '0908 877 6655' "/>  
                     </div>
               </div>
               
               <div className="WhatsApp">
                   <div className="name">
                       {/* <label for ="name">WhatsApp Number</label> */}
-                      <input type="email" className="" placeholder="WhatsApp '0908 877 6655'" />  
+                      <input type="tel" className={inputFormStyle} placeholder="WhatsApp '0908 877 6655'" />  
                   </div>
               </div>
 
               <div className="unit">
                   <div className="name">
                       {/* <label for ="name">Pick Your Unit in The Media</label> */}
-                        <select name="" id="">
+                        <select name="" id="" className={inputFormStyle}>
                             <option value="Content">Content </option>
                             <option value="Videography">Videography</option>
                             <option value="Photogrpahy">Photography</option>
@@ -63,7 +79,7 @@ const Form = () => {
                     </div>
               </div>
 
-              <button onClick="">Submit</button>
+              <button onClick="" className=" bg-[#3bb398] text-[#fafcff] mt-6 font-bold border rounded-xl p-2 w-24">Submit</button>
                   
               </form>
             
