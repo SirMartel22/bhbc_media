be78f311dfc0b593f09463a67b9009b7cac999ef
Sirmartel <oyinloyematthew6@gmail.com>
2025-08-16 22:59:28 +0100

    Feat: Update

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const express = require('express');
+const express = require('express')
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json(
-        {
-            "users":
-                ["userOne", "userTwo", "userThree"]
-        }
-    )
-    console.log(res.json());
+const port = 5000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Word')
 })
 
-const port = 5000;
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
