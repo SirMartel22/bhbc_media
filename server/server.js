86dd1543f908829f3e8303a567b7f304203855e2
Sirmartel <oyinloyematthew6@gmail.com>
2025-09-22 14:27:25 +0100

    Chore: Added vercel link to the cors

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 //configuring middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["https://bhbc-media.vercel.app/", "http://127.0.0.1:5173"],
     credentials: true,
 }))
 app.use(express.urlencoded({
